891203b4e6de3a771779736ea333f3d8d6d50fba
unknown <782468678@qq.com>
2026-09-19 10:44:17 +0800

    Add blue company desktop icon

--- starcompany_integration/hooks.py
+++ starcompany_integration/hooks.py
@@ -4,6 +4,17 @@ app_publisher = "Skychip"
 app_description = "Native ERPNext integration boundary for Starcompany"
 app_email = ""
 app_license = "MIT"
+app_logo_url = "/assets/starcompany_integration/images/starcompany.svg"
+app_home = "/app/starcompany"
+
+add_to_apps_screen = [
+	{
+		"name": app_name,
+		"logo": app_logo_url,
+		"title": "Starcompany",
+		"route": app_home,
+	}
+]
 
 after_install = "starcompany_integration.install.after_install"
 after_migrate = "starcompany_integration.install.ensure_workspace"
